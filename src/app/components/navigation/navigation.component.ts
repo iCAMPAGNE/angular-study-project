0b743b810ce0b5073c1014c7482b5bc9b95a9673
iCAMPAGNE <development@icampagne.nl>
2023-10-19 15:56:27 +0200

    v0.0.2 Added navigation.

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'navigation-menu',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit{
+  currentPage: string = '';
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    console.log(this.router.url);
+    this.currentPage = this.router.url.split('/')[1];
+    console.log(this.currentPage);
+  }
+
+}
